f67aceb19d97c24041a4ebd8a524a64133aa1fed
Denis-Zenkov <Denis-olegov@mail.ru>
2022-02-27 11:17:01 +0300

    Init2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,17 +3,18 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.CarService;
 import web.model.Car;
+import web.service.CarService;
 
 import java.util.List;
 
 @Controller
 public class CarController {
-    private CarService carService = new CarService();
+    private CarService carService;
+    public CarController(CarService carService){
+        this.carService = carService;
+    }
 
     @GetMapping("/cars")
     public String getCars(@RequestParam("count") int count, Model model) {
